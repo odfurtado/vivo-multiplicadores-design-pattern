9636357e8648582d8b849e2afef41a51b639ced5
odfurtado <odfurtado@gmail.com>
2023-03-08 16:29:09 -0300

    1 file modified

--- application/validator/ValidateSalesOrderRequest.ts
+++ application/validator/ValidateSalesOrderRequest.ts
@@ -8,10 +8,10 @@ export default class ValidateSalesOrderRequest extends OrderValidator {
       if (!order.items || order.items.length < 2) {
         throw new Error('A ordem de venda precisa de no minimo 2 itens.');
       }
-      const sum = order.items.reduce((currentSum, currentItem) => {
-        currentSum += currentItem.value;
-        return currentSum;
-      }, 0);
+      const sum = order.items.reduce(
+        (currentSum, currentItem) => currentSum + currentItem.value,
+        0
+      );
       if (sum < 30) {
         throw new Error('A ordem de venda precisa ser de no minimo R$30,00');
       }
